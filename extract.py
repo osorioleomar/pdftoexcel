c5efa675bab3f29040431ad5124910fa84af9e9f
Leomar Osorio <osorioleomar@gmail.com>
2023-09-27 22:11:23 +0530

    Added an incomplete script
    This is for automatically extracting all texts from all files inside a folder.

--- extract.py
+++ extract.py
@@ -1,21 +1,65 @@
+import os
 import PyPDF2
 import openpyxl
 
-# Create an Excel workbook
-wb = openpyxl.Workbook()
+def extract_text_from_pdf(pdf_file):
+  """Extracts all the text from a PDF file.
 
-# Open the PDF file
-pdf_file = PyPDF2.PdfReader("csr2017e_print.pdf")
+  Args:
+    pdf_file: The path to the PDF file.
 
-# Iterate over the pages in the PDF
-for page_num in range(len(pdf_file.pages)):
+  Returns:
+    A string containing all the text in the PDF file.
+  """
 
-    # Get the text from the current page
-    page_text = pdf_file.pages[page_num].extract_text()
+  pdf_reader = PyPDF2.PdfReader(pdf_file)
+  text = ""
+  for page in pdf_reader.pages:
+    text += page.extract_text()
 
-    # Write the text to the Excel workbook
-    ws = wb.active
-    ws.cell(row=page_num + 1, column=1).value = page_text
+  # Replace all occurrences of the character "◦" with an empty string.
+  text = text.translate(str.maketrans("", "", "◦"))
 
-# Save the Excel workbook
-wb.save("csr2017e_print.xlsx")
+  return text
+
+def create_excel_file(excel_file_path):
+  """Creates an Excel file with a single worksheet.
+
+  Args:
+    excel_file_path: The path to the Excel file.
+  """
+
+  workbook = openpyxl.Workbook()
+  worksheet = workbook.active
+  worksheet.title = "Text"
+  workbook.save(excel_file_path)
+
+def write_text_to_excel_file(excel_file_path, text):
+  """Writes text to an Excel file.
+
+  Args:
+    excel_file_path: The path to the Excel file.
+    text: The text to write to the Excel file.
+  """
+
+  workbook = openpyxl.load_workbook(excel_file_path)
+  worksheet = workbook.active
+  worksheet.append(text.split("\n"))
+  workbook.save(excel_file_path)
+
+if __name__ == "__main__":
+  pdf_folder_path = "PDF"
+  pdf_files = os.listdir(pdf_folder_path)
+
+  for pdf_file in pdf_files:
+    # Extract the text from the PDF file.
+    text = extract_text_from_pdf(os.path.join(pdf_folder_path, pdf_file))
+
+    # Create an Excel file with the same filename as the PDF file.
+    excel_file_path = os.path.join(pdf_folder_path, pdf_file.replace(".pdf", ".xlsx"))
+    create_excel_file(excel_file_path)
+
+    # Write the text to the Excel file.
+    write_text_to_excel_file(excel_file_path, text)
+
+    print("Extracted the text from {} to {}".format(pdf_file, excel_file_path))
